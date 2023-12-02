98043b1e1babc40aa9e46610cf9f20beda39413a
V1rtualAlloc <kocovic.vladimir@gmail.com>
2023-12-02 23:12:31 +0100

    harvester model update

--- model/harvester_model.py
+++ model/harvester_model.py
@@ -1,86 +1,41 @@
-import json
-import os
-import mysql.connector
-import requests
-from bs4 import BeautifulSoup
-import time
+from services.data_serializer_service import DataSerializerService
+from services.download_service import DownloadService
+from services.parse_service import ParseService
+from services.database_service import DatabaseService
+
 from model.base_model import BaseModel
 
 
 class HarvesterModel(BaseModel):
-    def download_html(self, url):
-        response = requests.get(url)
-        return response.content
-    
-    def parse_html(self, html_content):
-        # Your parsing logic here using BeautifulSoup or any other library
-        # Example:
-        soup = BeautifulSoup(html_content, 'html.parser')
-        # Extract data from the parsed HTML
-        data = {'example': 'data'}
-        return data
-    
-    def insert_into_mariadb(data, cursor):
-        # Your MariaDB insertion logic here
-        # Example (replace this with your actual MariaDB insertion code):
-        query = "INSERT INTO your_table (column1, column2) VALUES (%s, %s)"
-        values = (data['example'], 'some_value')
-        cursor.execute(query, values)
+    def __init__(self, pages, name, template, output_directory='output_data'):
+        self.pages = pages
+        self.name = name
+        self.template = template
+        self.output_directory = output_directory
 
-    def serialize_progress(self, current_page, output_directory):
-        progress = {'current_page': current_page}
-        with open(os.path.join(output_directory, 'progress.json'), 'w') as f:
-            json.dump(progress, f, indent=2)
+    def process_pages(self):
+        # Initialize DataSerializerService
+        serializer = DataSerializerService(self.pages, self.name)
 
-    def start(self, database_name, ):
-        # Your main processing loop
-        output_directory = "output_data"
-        if not os.path.exists(output_directory):
-            os.makedirs(output_directory)
+        # Iterate through pages
+        for current_page, page_url in enumerate(self.pages, start=1):
+            with DownloadService() as download_service:
+                # Download page
+                html_content, response = download_service.download_page(page_url)
 
-        # MariaDB connection details
-        database_config = {
-            'host': 'your_host',
-            'user': 'your_user',
-            'password': 'your_password',
-            'database': 'your_database',
-        }
+            if response.ok and html_content is not None:
+                with ParseService(html_content, self.template) as parse_service:
+                    # Parse HTML
+                    parsed_data = parse_service.parse_html()
 
-        connection = mysql.connector.connect(**database_config)
-        cursor = connection.cursor()
+                with DatabaseService() as db_service:
+                    # Add parsed data to database
+                    query = "INSERT INTO your_table (column1, column2) VALUES (%s, %s)"
+                    params = (parsed_data.get('key1', ''), parsed_data.get('key2', ''))
+                    db_service.execute_query(query, params)
 
-        # Create your MariaDB table if it doesn't exist
-        cursor.execute("CREATE TABLE IF NOT EXISTS your_table (column1 VARCHAR(255), column2 VARCHAR(255))")
+                # Update progress and serialize
+                serializer.processed_pages.append(current_page)
+                serializer.serialize_progress(current_page, self.output_directory)
 
-        # Load progress if it exists
-        progress_file = os.path.join(output_directory, 'progress.json')
-        if os.path.exists(progress_file):
-            with open(progress_file, 'r') as f:
-                progress = json.load(f)
-            start_page = progress['current_page'] + 1
-        else:
-            start_page = 1
-
-        for current_page in range(start_page, 10001):
-            # Download HTML
-            url = f"your_base_url/page{current_page}.html"
-            html_content = self.download_html(url)
-
-            # Parse HTML
-            parsed_data = self.parse_html(html_content)
-
-            # Insert into MariaDB
-            self.insert_into_mariadb(parsed_data, cursor)
-            connection.commit()
-
-            # Serialize progress
-            self.serialize_progress(current_page, output_directory)
-
-            print(f"Processed and saved page {current_page}.")
-
-            # Add a 5-second delay between processing pages
-            time.sleep(5)
-
-        # Close the MariaDB connection
-        connection.close()
 
